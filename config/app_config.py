5fc9b529b55e870e779d70a750feef5a4b1a4ba6
Leonardo Catello <leonardo.catello23@gmail.com>
2025-07-11 02:11:07 +0200

    workflow version update - prompt engineering

--- config/app_config.py
+++ config/app_config.py
@@ -26,7 +26,7 @@ class AppConfig:
 
     # Workflow configuration settings
     WORKFLOW_NAME = "WanderFlow"
-    WORKFLOW_VERSION = 27
+    WORKFLOW_VERSION = 28
 
     # User interface configuration
     MAX_ATTEMPTS = 30
